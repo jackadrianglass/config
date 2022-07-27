063add43fc7bc8953d5b506aa721139213d11f59
Jack Glass <jackadrianglass@gmail.com>
2022-07-26 21:47:02 -0600

    Change lsp installer setup to recommendation in quickstart

--- lua/plugins/lsp/lsp-installer.lua
+++ lua/plugins/lsp/lsp-installer.lua
@@ -2,23 +2,10 @@ local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
 if not status_ok then
   return
 end
+handlers = require('plugins.lsp.handlers')
+lspconfig = require('lspconfig')
 
--- Registers all of the servers then they're ready
-lsp_installer.on_server_ready(function(server)
-  local opts = {
-    on_attach = require('plugins.lsp.handlers').on_attach,
-    capabilities = require('plugins.lsp.handlers').capabilities,
-  }
-
-  if server.name == 'jsonls' then
-    local jsonls_opts = require('plugins.lsp.settings.jsonls')
-    opts = vim.tbl_deep_extend('force', jsonls_opts, opts)
-  end
-
-  if server.name == 'sumneko_lua' then
-    local sumneko_opts = require('plugins.lsp.settings.sumneko_lua')
-    opts = vim.tbl_deep_extend('force', sumneko_opts, opts)
-  end
-
-  server:setup(opts)
-end)
+lsp_installer.setup {}
+lspconfig.sumneko_lua.setup {on_attach = handlers.on_attach }
+lspconfig.elmls.setup {on_attach = handlers.on_attach }
+lspconfig.rust_analyzer.setup {on_attach = handlers.on_attach }
